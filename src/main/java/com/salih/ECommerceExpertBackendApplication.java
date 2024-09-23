908202071cb95a6f3df8a6108a4844bb401ca9b1
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2024-09-23 23:50:00 +0300

    Initial commit

---
+++ src/main/java/com/salih/ECommerceExpertBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceExpertBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceExpertBackendApplication.class, args);
+    }
+
+}
