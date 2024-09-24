1293e6d068ddc47241e41921af493a4924e76b85
Abdullah Salih Öner <64440843+AbdullahSalihOner@users.noreply.github.com>
2024-09-24 23:15:22 +0300

    first commit

--- src/main/java/com/salih/AppMain.java
+++ src/main/java/com/salih/AppMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ECommerceExpertBackendApplication {
+public class AppMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(ECommerceExpertBackendApplication.class, args);
+        SpringApplication.run(AppMain.class, args);
     }
 
 }
